b3c75c1e658b7a900a0436f5de305aab46c6809f
Rodger Visitacion <rodgerv@Rodgers-MacBook-Pro.local>
2014-08-26 14:10:58 -0700

    Initial Commit

---
+++ UITableViewController Challenge Solution/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Rodger Visitacion on 8/26/14.
+//  Copyright (c) 2014 Live Well Sports. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Rodger Visitacion on 8/26/14.
+//  Copyright (c) 2014 Live Well Sports. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
